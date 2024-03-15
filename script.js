64be95e4a734051420ad73b06821a024728ccdb9
chafrak <zakary.walter15@gmail.com>
2024-03-15 10:42:33 +0100

    transition abput

--- script.js
+++ script.js
@@ -1,51 +1,102 @@
-function sortir_tirroir(){
+document.addEventListener("DOMContentLoaded", (event) => {
     var tirroir = document.getElementById("tirroir");
-    var cont2 = document.getElementById("cont2");
     var cont = document.getElementById("cont");
+    var cont2 = document.getElementById("cont2");
+    var cont3 = document.getElementById("cont3");
+    var navbox = document.getElementById("navbox");
+
+    cont.classList.add("accueil");
+    cont2.classList.add("accueil");
+    cont3.classList.add("accueil");
+});
+
+function sortir_tirroir(){
     tirroir.classList.add("sorti");
     cont.classList.add("sorti");
     cont2.classList.add("sorti");
-
+    cont3.classList.add("sorti");
 }
 
 function ranger_tirroir(){
-    var tirroir = document.getElementById("tirroir");
-    var cont2 = document.getElementById("cont2");
-    var cont = document.getElementById("cont");
     cont.classList.remove("sorti");
     cont2.classList.remove("sorti");
+    cont3.classList.remove("sorti");
     tirroir.classList.remove("sorti");
 }
 
-// function box_agrandit() {
-//     var box = document.getElementById("box");
-//     box.classList.add("box_grande")
-// }
-
-// function box_retreci() {
-//     var box = document.getElementById("box");
-//     box.classList.remove("box_grande")
-// }
 function box_agrandit() {
-    var navbox = document.getElementById("navbox");
     navbox.classList.add("navbox_grande")
 }
 
 function box_retreci() {
-    var navbox = document.getElementById("navbox");
     navbox.classList.remove("navbox_grande")
 }
 
 function go_to_contact() {
-    var cont = document.getElementById("cont");
-    var cont2 = document.getElementById("cont2");
-    cont.classList.add("switch");
-    cont2.classList.add("switch");
+    if (cont.classList.contains("about")) {
+        go_to_accueil();
+        setTimeout(() => {
+            cont.classList.add("contact");
+            cont2.classList.add("contact");
+            cont3.classList.add("contact");
+            cont.classList.remove("about");
+            cont2.classList.remove("about");
+            cont3.classList.remove("about");
+            cont.classList.remove("accueil");
+            cont2.classList.remove("accueil");
+            cont3.classList.remove("accueil");
+        }, 300);
+    }
+    else {
+        cont.classList.add("contact");
+        cont2.classList.add("contact");
+        cont3.classList.add("contact");
+        cont.classList.remove("about");
+        cont2.classList.remove("about");
+        cont3.classList.remove("about");
+        cont.classList.remove("accueil");
+        cont2.classList.remove("accueil");
+        cont3.classList.remove("accueil");
+    }
 }
 
 function go_to_accueil() {
-    var cont = document.getElementById("cont");
-    var cont2 = document.getElementById("cont2");
-    cont.classList.remove("switch");
-    cont2.classList.remove("switch");
+    cont.classList.remove("contact");
+    cont2.classList.remove("contact");
+    cont3.classList.remove("contact");
+    cont.classList.remove("about");
+    cont2.classList.remove("about");
+    cont3.classList.remove("about");
+    cont.classList.add("accueil");
+    cont2.classList.add("accueil");
+    cont3.classList.add("accueil");
+}
+
+function go_to_about() {
+    if (cont.classList.contains("contact")) {
+        go_to_accueil();
+        setTimeout(() => {
+            cont.classList.add("about");
+            cont2.classList.add("about");
+            cont3.classList.add("about");
+            cont.classList.remove("contact");
+            cont2.classList.remove("contact");
+            cont3.classList.remove("contact");
+            cont.classList.remove("accueil");
+            cont2.classList.remove("accueil");
+            cont3.classList.remove("accueil");
+        }, 300);
+    }
+    else {
+        cont.classList.add("about");
+        cont2.classList.add("about");
+        cont3.classList.add("about");
+        cont.classList.remove("contact");
+        cont2.classList.remove("contact");
+        cont3.classList.remove("contact");
+        cont.classList.remove("accueil");
+        cont2.classList.remove("accueil");
+        cont3.classList.remove("accueil");
+    }
+
 }
